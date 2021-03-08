57a2886845c4f697b1bdf54e259102dd841f2146
Carlos Pintos <cpintos@makingsense.com>
2021-03-08 18:19:23 -0300

    [DE-107] Authorization fix

--- DopplerFiles/DopplerSecurity/DopplerSecurityDefaults.cs
+++ DopplerFiles/DopplerSecurity/DopplerSecurityDefaults.cs
@@ -2,10 +2,9 @@ namespace DopplerFiles.DopplerSecurity
 {
     public static class DopplerSecurityDefaults
     {
-        public const string DEFAULT_OR_SIGNED_PATHS_POLICY = "DefaultOrSignedPathsPolicy";
         public const string PUBLIC_KEYS_FOLDER_CONFIG_KEY = "PublicKeysFolder";
         public const string PUBLIC_KEYS_FOLDER_DEFAULT_CONFIG_VALUE = "public-keys";
         public const string SUPERUSER_JWT_KEY = "isSu";
-        public const string USER_JWT_KEY = "unique_name";
+        public const string USER_JWT_KEY = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
     }
 }
